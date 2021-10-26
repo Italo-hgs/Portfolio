a1aafa91275b9d57932490f7ef535cc9e1843e5e
Italo Henrique Góes da Silva <70420128+Italo-hgs@users.noreply.github.com>
2021-10-26 15:28:39 -0300

    Add files via upload

---
+++ Portfolio/README.md
@@ -0,0 +1,4 @@
+# Portfolio
+Olá, esse é o meu portfólio, senta-se livre para baixa-lo e testa-lo na sua máquina, acesse o link abaixo e explore este projeto!!
+# Link
+italogoes.netlify.app
---
+++ Portfolio/js/script.js
@@ -0,0 +1,17 @@
+function abrirMenu(){
+    var x = document.getElementById('open');
+    if(x.style.display === 'block'){
+        x.style.display = 'none';
+    }else{
+        x.style.display = 'block';
+    }
+}
+
+function fecharMenu(){
+    var a = document.getElementById('open');
+    if(a.style.display === 'block'){
+        a.style.display = 'none';
+    }else{
+        a.style.display = 'block';
+    }
+}
